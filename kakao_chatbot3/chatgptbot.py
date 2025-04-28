7594d3628b239b046fd77a1e5d5b2a0ab42d6dd6
sw-jooyeon <juyeon0716@naver.com>
2025-04-28 16:55:09 +0900

    2nd commit

--- kakao_chatbot3/chatgptbot.py
+++ kakao_chatbot3/chatgptbot.py
@@ -3,8 +3,12 @@
 from flask import Flask, jsonify, request
 from openai import OpenAI
 from bs4 import BeautifulSoup
+from PIL import Image
 import requests
 import threading
+import json
+import pytesseract
+import io
 
 # Flask 앱 객체 생성
 app = Flask(__name__)
@@ -64,41 +68,9 @@ def chat_response():
     user_request = request.json.get('userRequest', {})
     callback_url = user_request.get('callbackUrl')
     utterance = user_request.get('utterance', '')
-    print(f"user_request: {user_request}")
-    print(f"callback_url: {callback_url}")
-    print(f"utterance: {utterance}")
     
     # 비동기 처리
     threading.Thread(target=gpt_reply, args=(callback_url, utterance)).start()
-
-    # # 시스템 컨텍스트로 메시지 리스트 초기화
-    # messageList = [{"role": "system", "content": CHAT_MODEL_CONTEXT}]
-
-    # # 사용자 메시지를 메시지 리스트에 추가
-    # dicMessage = {"role": "user", "content": utterance}
-    # messageList.append(dicMessage)
-
-    # # OpenAI 채팅 모델에서 응답 받기
-    # completion = client.chat.completions.create(
-    #     model="gpt-4o",
-    #     messages=messageList
-    # )
-    # response_message = completion.choices[0].message
-
-    # # 응답 형식 구성
-    # response = {
-    #     "version": "2.0",
-    #     "template": {
-    #         "outputs": [
-    #             {
-    #                 "simpleText": {
-    #                     "text": f"{response_message.content}"
-
-    #                 }
-    #             }
-    #         ]
-    #     }
-    # }
 
     #JSON 응답 반환
     return jsonify({
@@ -115,9 +87,6 @@ def chat_summary():
     user_request = request.json.get('userRequest', {})
     callback_url = user_request.get('callbackUrl')
     utterance = user_request.get('utterance', '')
-    print(f"user_request: {user_request}")
-    print(f"callback_url: {callback_url}")
-    print(f"utterance: {utterance}")
     
     # 웹페이지 요청
     res = requests.get(utterance)
@@ -135,43 +104,58 @@ def chat_summary():
     
     # 비동기 처리
     threading.Thread(target=gpt_reply, args=(callback_url, text_content, True)).start()
-    
-    # # 시스템 컨텍스트로 메시지 리스트 초기화
-    # messageList = [
-    #     {"role": "system", "content": CHAT_MODEL_CONTEXT},
-    #     {"role": "system", "content": f"다음 웹 페이지의 내용을 요약해줘."}
-    # ]
-
-    # # 사용자 메시지를 메시지 리스트에 추가
-    # dicMessage = {"role": "user", "content": text_content}
-    # messageList.append(dicMessage)
-
-    # # OpenAI 채팅 모델에서 응답 받기
-    # completion = client.chat.completions.create(
-    #     model="gpt-4o",
-    #     messages=messageList
-    # )
-    # response_message = completion.choices[0].message
-
-    # # 응답 형식 구성
-    # response = {
-    #     "version": "2.0",
-    #     "template": {
-    #         "outputs": [
-    #             {
-    #                 "simpleText": {
-    #                     "text": f"{response_message.content}"
-
-    #                 }
-    #             }
-    #         ]
-    #     }
-    # }
 
     #JSON 응답 반환
     return jsonify({
       "version" : "2.0",
       "useCallback" : True
+    })
+
+
+@app.route('/img', methods=['POST'])
+def img_txt():
+    
+    # 사용자 요청 데이터 추출
+    user_request = request.json.get('userRequest', {})
+    # callback_url = user_request.get('callbackUrl')
+    # utterance = user_request.get('utterance', '')
+    
+    # 액션 파라미터
+    action = request.json.get('action', {})
+    params = action.get('params', {})
+    secureimg_raw = params.get('secureimage', '')
+
+    # JSON 파싱
+    secureimg_data = json.loads(secureimg_raw)
+    secure_urls_str = secureimg_data.get('secureUrls', '')
+
+    # URL 추출
+    urls = secure_urls_str[5:-1].split(', ')
+    img_url = urls[0]
+
+    # 이미지 다운로드
+    img_res = requests.get(img_url)
+    img = Image.open(io.BytesIO(img_res.content))
+
+    # OCR
+    extracted_txt = pytesseract.image_to_string(img, lang='kor+eng')
+
+    # 비동기 처리
+    # threading.Thread(target=gpt_reply, args=(callback_url, utterance)).start()
+    
+    #JSON 응답 반환
+    return jsonify({
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": f"{extracted_txt}"
+
+                    }
+                }
+            ]
+        }
     })
 
 
